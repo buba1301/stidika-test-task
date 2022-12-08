03d15188c7e88ca240517704f41b776325f50d4b
Boris Muzalevskii <bmuza57@gmail.com>
2022-12-08 20:40:31 +0300

    Add js for dropdown

--- cities.js
+++ cities.js
@@ -10,7 +10,24 @@ const cities = [
 ];
 
 export function setUpCitiesItems(element) {
-  return cities.forEach((city) => {
+  let state = {
+    bageList: [],
+  };
+
+  const isEmptyBageList = state.bageList.length === 0;
+
+  const dropDownElement = document.querySelector('.drop_down_menu');
+  const bagesContainer = document.querySelector('.cities-bages');
+
+  if (isEmptyBageList) {
+    dropDownElement.style.cssText = 'height: 340px';
+    bagesContainer.classList.add('hidden');
+  } else {
+    dropDownElement.style.cssText = 'height: 420px';
+    bagesContainer.classList.remove('hidden');
+  }
+
+  cities.forEach((city) => {
     const itemElement = document.createElement('div');
     const spanElement = document.createElement('span');
 
@@ -18,6 +35,17 @@ export function setUpCitiesItems(element) {
     itemElement.appendChild(spanElement);
     itemElement.classList.add('city');
 
+    itemElement.addEventListener('click', (event) => {
+      console.log('item', event.target.innerText);
+      const bageName = event.target.innerText;
+      state = {
+        ...state,
+        bageList: [...state.bageList, event.target.innerText],
+      };
+    });
+
     element.appendChild(itemElement);
   });
+
+  console.log('state', state);
 }
